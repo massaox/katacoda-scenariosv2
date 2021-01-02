0ae86983f330a67ec650b8944f89782856710894
root <rafael.tanaka@jetstack.io>
2021-01-02 12:23:09 +0000

    testing bg

--- kipday2/hpa/background.sh
+++ kipday2/hpa/background.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Starting Kubernetes" > /opt/.launch
+echo "Starting Kubernetes cluster" >&2
 
 while [ `kubectl get nodes | grep " Ready" |  wc -l` -ne 2 ]
 do
   sleep 1
 done
 
-echo "Kubernetes started" > /opt/.launc
-
+echo "Kubernetes started" >&2
+ 
 kubectl apply -f /root/setup/metrics-server_0.4.1.yaml
